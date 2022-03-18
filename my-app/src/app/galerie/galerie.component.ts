feeeeaceb520b1938cdfdf6fb61661a36b6921a1
antoinedelia <antoine.delia@gmail.com>
2022-03-18 22:57:27 +0100

    Finalize last pages

--- my-app/src/app/galerie/galerie.component.ts
+++ my-app/src/app/galerie/galerie.component.ts
@@ -7,6 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalerieComponent implements OnInit {
 
+  photos = [
+    {
+      src: "assets/img/neige.JPG",
+      alt: "Neige au cabinet",
+      title: "Le cabinet sous la neige"
+    },
+    {
+      src: "assets/img/arbre.JPG",
+      alt: "Arbres devant le cabinet",
+      title: "Arbres devant le cabinet"
+    },
+    {
+      src: "assets/img/entree.jpg",
+      alt: "L'entrée du cabinet",
+      title: "L'accueil du cabinet d'Auterive"
+    },
+    {
+      src: "assets/img/bureau.jpg",
+      alt: "Le bureau du Docteur Delia",
+      title: "Bureau du Docteur DELIA"
+    },
+    {
+      src: "assets/img/cabinet3.jpeg",
+      alt: "Salle de soin du Docteur Delia",
+      title: "Salle de soin du Docteur Delia"
+    },
+    {
+      src: "assets/img/cabinet4.jpeg",
+      alt: "Salle de soin du Docteur Sperte",
+      title: "Salle de soin du Docteur Sperte"
+    },
+    {
+      src: "assets/img/equipe.jpg",
+      alt: "L'équipe du cabinet",
+      title: "L'équipe du cabinet"
+    },
+    {
+      src: "assets/img/delia_masque.jpg",
+      alt: "Nous sommes équipés pour vous recevoir !",
+      title: "Nous sommes équipés pour vous recevoir !"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
