9a4dc6947ee6786d3989c95adfddfa88015d22d1
antoinedelia <antoine.delia@gmail.com>
2022-03-22 22:17:49 +0100

    Add Terraform configuration

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,45 @@
+resource "aws_cloudfront_distribution" "dist" {
+  origin {
+    domain_name = aws_s3_bucket.site.website_endpoint
+    origin_id   = aws_s3_bucket.site.id
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = [
+    var.domain_name, "www.${var.domain_name}"
+  ]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.site.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method  = "sni-only"
+  }
+}
