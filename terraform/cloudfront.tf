12af4214c53e03299cd3609b0a1f5ab4f93ca825
antoinedelia <antoine.delia@gmail.com>
2022-03-24 12:00:46 +0100

    Add Pierre

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -29,9 +29,6 @@ resource "aws_cloudfront_distribution" "dist" {
       }
     }
     viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
   viewer_certificate {
