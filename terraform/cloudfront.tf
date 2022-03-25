3a1ec1ca7a05c65413b02ddd94507e4cdfe08ea8
antoinedelia <antoine.delia@gmail.com>
2022-03-25 17:20:49 +0100

    Redirect to HTTPS

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -30,7 +30,7 @@ resource "aws_cloudfront_distribution" "dist" {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
