2a5ab1129c907b48835cdebbcdfa5559f73bcf4e
Antoine Delia <antoine.delia@gmail.com>
2023-02-17 15:46:27 +0100

    Better way to display team

--- web/src/app/equipe/equipe.component.ts
+++ web/src/app/equipe/equipe.component.ts
@@ -9,6 +9,74 @@ import { VariableService } from '../variables.service';
 })
 export class EquipeComponent implements OnInit {
 
+  members = [
+    {
+      name: "Christophe Delia",
+      img: "delia",
+      descriptions: [
+        "Docteur en chirurgie dentaire",
+        "Diplomé de la faculté de Toulouse",
+        "Diplôme d'État de manipulateur d'électroradiologie médicale",
+        "Diplôme universitaire d'odontologie légale et expertise",
+        "Pratique l'implantologie depuis 2004",
+        "Certificat de formation à l'orthodontie invisible par gouttières transparentes SMILERS® - Toulouse 2018",
+        "Certificat de formation à l'utilisation et à l'optimisation du ConeBeam en imagerie médicale - Toulouse 2018",
+        "Certificat de prothèses sur implant Dentsply - Toulouse 2008",
+        "Certificat anesthésie sans douleur Quicksleeper - Toulouse 2006",
+        "Certificat de suivi de l'enseignement magistral et pratique en biomatériaux et implantologie - Angers 2006",
+      ],
+    },
+    {
+      name: "Marion Sperte",
+      img: "sperte",
+      descriptions: [
+        "Docteur en chirurgie dentaire",
+        "Diplomée de la faculté de Toulouse",
+        "Certificat de formation à l'orthodontie invisible par gouttières transparentes SMILERS® - Toulouse 2017",
+      ],
+    },
+    {
+      name: "Paul Aragon",
+      img: "aragon",
+      descriptions: [
+        "Docteur en chirurgie dentaire",
+        "Diplomé de la faculté de Toulouse",
+      ],
+    },
+    {
+      name: "Pierre Mérieux",
+      img: "pierre",
+      descriptions: [
+        "Docteur en chirurgie dentaire",
+        "Diplomé de la faculté de Toulouse",
+      ],
+    },
+    {
+      name: "Mégane",
+      img: "megane",
+      descriptions: [
+        "Assistante dentaire qualifiée depuis 2018",
+        "Diplomée de l'école AGORA Toulouse",
+        "Responsable accueil, suivi des patients et comptabilité",
+      ],
+    },
+    {
+      name: "Laura",
+      img: "laura",
+      descriptions: [
+        "Assistante dentaire qualifiée depuis 2016",
+        "Diplomée de l'ESAD Toulouse",
+      ],
+    },
+    {
+      name: "Alizée",
+      img: "alizee",
+      descriptions: [
+        "Assistante dentaire en formation",
+      ],
+    },
+  ]
+
   constructor(private variableService: VariableService, private title: Title) {
     const staticTitle = this.variableService.getTitle();
     this.title.setTitle("L'équipe | " + staticTitle);
