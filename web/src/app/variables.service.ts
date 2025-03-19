5fbdb23bb072d17c375b6111ab6822b43b40e292
Antoine Delia <antoine.delia@gmail.com>
2025-03-19 17:48:38 +0100

    feat: replace aragon by regis

--- web/src/app/variables.service.ts
+++ web/src/app/variables.service.ts
@@ -1,7 +1,7 @@
 
 
 export class VariableService {
-    private siteTitle: string = "Dentiste Auterive 31190 Cabinet dentaire des Drs DELIA, ARAGON et MERIEUX";
+    private siteTitle: string = "Dentiste Auterive 31190 Cabinet dentaire des Drs DELIA, MERIEUX et REGIS";
 
     getTitle(): string {
         return this.siteTitle;
