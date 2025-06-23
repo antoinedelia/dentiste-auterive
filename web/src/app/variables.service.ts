f4281c3f839c5b1be851179bb4d2f8bb537e9d17
Antoine Delia <antoine.delia@gmail.com>
2025-06-23 19:03:30 +0200

    chore: move to web folder

--- web/src/app/variables.service.ts
+++
@@ -1,9 +0,0 @@
-
-
-export class VariableService {
-    private siteTitle: string = "Dentiste Auterive 31190 Cabinet dentaire des Drs DELIA, MERIEUX et REGIS";
-
-    getTitle(): string {
-        return this.siteTitle;
-    }
-}
